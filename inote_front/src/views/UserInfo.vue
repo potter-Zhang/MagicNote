f996f0755d0f2a7d12e2c4bcc0ee8dbc5f238f5d
eumendies <1431538355@qq.com>
2023-11-13 21:57:21 +0800

    完成用户信息界面框架

--- inote_front/src/views/UserInfo.vue
+++ inote_front/src/views/UserInfo.vue
@@ -1,5 +1,27 @@
 <script setup>
   import back from "@icon-park/vue-next/lib/icons/Back"
+  import me from "@icon-park/vue-next/lib/icons/Me"
+  import setting from "@icon-park/vue-next/lib/icons/Setting";
+
+  const date = new Date();
+  // 用于展示在时间线里的数据
+  const activities = [
+    {
+      content: '创建了...',
+      timestamp: date.toDateString(),
+      type: 'add'
+    },
+    {
+      content: '修改了...',
+      timestamp: '2018-04-13',
+      type: 'update'
+    },
+    {
+      content: '删除了...',
+      timestamp: '2018-04-11',
+      type: 'delete'
+    }
+  ]
 </script>
 
 <template>
@@ -11,13 +33,49 @@
       </div>
     </el-header>
 
-    <el-main id="body" style="height: 100%">
-      <router-link to="/"><back id="backIcon" theme="outline" size="24" fill="#ffffff"/></router-link>
+    <el-main id="body" style="height: 100%; padding: 0">
+      <div style="display: flex; justify-content: center">
+        <router-link to="/"><back id="backIcon" theme="outline" size="24" fill="#ffffff"/></router-link>
+        <div style="display: flex; flex-direction: column; flex: 1; align-items: center">
+          <div id="userInfoCard">
+            <me theme="outline" size="128" fill="#333" style="margin-left: 5%"/>
+            <div id="info">
+              <div>用户名：test</div>
+              <div>个人介绍：test test test</div>
+            </div>
+            <div id="settingButton">
+              <setting theme="outline" size="24" fill="#000000"/>
+              <span style="margin-left: 5px">个人设置</span>
+            </div>
+          </div>
+
+          <div id="beneath" style="width: 55%; margin-top: 10px">
+            <div style="font-size: 20px">动态</div>
+            <el-timeline id="timeline">
+              <el-timeline-item v-for="(activity, index) in activities" :key="index" :timestamp="activity.timestamp" :placement="'top'">
+                <el-card>
+                  <template #header>
+                    <div style="display: flex; justify-content: flex-end; font-size: 10px; color: rgb(0,0,0,0.3)">
+                      一天前
+                    </div>
+                  </template>
+                  {{activity.content}}
+                </el-card>
+              </el-timeline-item>
+            </el-timeline>
+          </div>
+        </div>
+      </div>
+
     </el-main>
   </el-container>
 </template>
 
 <style scoped>
+  html {
+    font-size: 10px;
+  }
+
   #header {
     display: flex;
     justify-content: space-between;
@@ -34,12 +92,46 @@
     background-color: #a5d63f;
     padding: 4px;
     cursor: pointer;
+    margin: 5px 0 0 5px;
   }
 
-  #infoBox {
-    height: 100px;
-    width: 90%;
-    background-color: #e9e9e9;
+  #userInfoCard {
+    width: 60%;
+    min-height: 200px;
+    background-color: rgb(204, 204, 204, 0.11);
     border-radius: 10px;
+    margin: 15px 0 0 20px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    overflow: hidden;
+  }
+
+  #info {
+    margin-left: 5%;
+    width: 50%;
+    height: 40%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+
+  #settingButton {
+    border-radius: 10px;
+    border: 1px solid #b8b8b8;
+    align-self: flex-start;
+    display: flex;
+    justify-content: center;
+    padding: 5px;
+    margin-top: 20px;
+    margin-right: 20px;
+    cursor: pointer;
+    min-width: 115px;
+  }
+
+  #timeline {
+    width: 100%;
+    padding-left: 20px;
+    margin-top: 20px;
   }
 </style>
