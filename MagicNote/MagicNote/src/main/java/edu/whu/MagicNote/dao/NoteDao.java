76e5aedfb4319822d1662d7a2f191d5130a8adb9
JeromeYJ <3064325896@qq.com>
2023-11-15 00:16:39 +0800

    添加editor的前端，用于测试

--- MagicNote/MagicNote/src/main/java/edu/whu/MagicNote/dao/NoteDao.java
+++ MagicNote/MagicNote/src/main/java/edu/whu/MagicNote/dao/NoteDao.java
@@ -3,12 +3,15 @@ package edu.whu.MagicNote.dao;
 import edu.whu.MagicNote.domain.Note;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
+
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
+
 
 /**
  * <p>
@@ -21,7 +24,13 @@ import java.util.List;
 
 @Mapper
 public interface NoteDao extends BaseMapper<Note> {
+    //根据文件名删除笔记
+    @Delete("DELETE FROM note WHERE note.name = #{name}")
+    void DeleteNoteByName(String name);
+    //根据文件名查询笔记
+    @Select("SELECT FROM user WHERE user.name = #{name}")
+    Note FindNoteByName(String name);
     //根据用户id查询所有笔记
     @Select("SELECT FROM note WHERE note.user_id = #{user_id}")
-    List<Note> FindAllNoteByUserId(int user_id);
+    List<Note> FindAllNoteByUserId(int id);
 }
