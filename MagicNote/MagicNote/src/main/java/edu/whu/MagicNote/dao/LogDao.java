6d1431d335426b32c642247773b39e07e2388fc0
JeromeYJ <3064325896@qq.com>
2023-11-15 14:43:50 +0800

    添加日志实体类

---
+++ MagicNote/MagicNote/src/main/java/edu/whu/MagicNote/dao/LogDao.java
@@ -0,0 +1,16 @@
+package edu.whu.MagicNote.dao;
+
+import edu.whu.MagicNote.domain.Log;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author Jerome
+ * @since 2023-11-15
+ */
+public interface LogDao extends BaseMapper<Log> {
+
+}
