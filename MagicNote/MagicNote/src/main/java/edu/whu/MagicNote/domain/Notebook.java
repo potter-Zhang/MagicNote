a17c464942f60c9b14f7b25c6f542e99ff5cc2e7
JeromeYJ <3064325896@qq.com>
2023-11-20 22:49:23 +0800

    添加notebook实体类与各层次文件

--- MagicNote/MagicNote/src/main/java/edu/whu/MagicNote/domain/Notebook.java
+++ MagicNote/MagicNote/src/main/java/edu/whu/MagicNote/domain/Notebook.java
@@ -2,23 +2,21 @@ package edu.whu.MagicNote.domain;
 
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
+import java.io.Serializable;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-
-import java.io.Serializable;
-import java.time.LocalDateTime;
 
 /**
  * <p>
  * 
  * </p>
  *
- * @author susong
- * @since 2023-11-14
+ * @author Jerome
+ * @since 2023-11-20
  */
 @Data
 @EqualsAndHashCode(callSuper = false)
-public class Note implements Serializable {
+public class Notebook implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
@@ -26,14 +24,6 @@ public class Note implements Serializable {
     private Integer id;
 
     private String name;
-
-    private Integer userid;
-
-    private String content;
-
-    private Integer notebookid;
-
-    private LocalDateTime createtime;
 
 
 }
