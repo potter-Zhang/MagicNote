a525afa1f07c194035b5a7f149e7fbc08cf744c7
eumendies <1431538355@qq.com>
2023-11-28 16:19:02 +0800

    在聊天框发送消息后自动滚动到底部

--- inote_front/src/components/DialogPanel.vue
+++ inote_front/src/components/DialogPanel.vue
@@ -1,12 +1,28 @@
 <script setup>
 import CustomDialog from './CustomDialog.vue'
 import arrowRight from "@icon-park/vue-next/lib/icons/ArrowRight";
-import { ref, computed } from 'vue'
+import { ref, computed, watch, nextTick } from 'vue'
 
 const isDragging = ref(false)
 const left = ref(500)
 const top = ref(300)
 const userMsg = ref('')
+
+
+const messages = ref([
+  {
+    role: 'assistant',
+    content: '您好，请问有什么可以帮助您的吗？'
+  },
+  {
+    role: 'user',
+    content: 'go say bye-bye'
+  },
+  {
+    role: 'user',
+    content: 'awohefiahfiehfoiewhfaiwfewahfewahefwaheflhwaelfhwjefh'
+  }
+])
 
 const leftPx = computed({
   get () {
@@ -40,29 +56,17 @@ function sendMessage (msg) {
   if (msg !== '') {
     messages.value.push({ role: 'user', content: msg })
     userMsg.value = "";
-    const container = document.getElementById("chat-container")
-    container.scrollTop = container.scrollHeight;
   }
 }
 
-const messages = ref([
-  {
-    role: 'assistant',
-    content: '您好，请问有什么可以帮助您的吗？'
-  },
-  {
-    role: 'user',
-    content: 'go say bye-bye'
-  },
-  {
-    role: 'user',
-    content: 'awohefiahfiehfoiewhfaiwfewahfewahefwaheflhwaelfhwjefh'
-  },
-  {
-    role: 'assistant',
-    content: '长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本'
-  }
-])
+watch(messages.value, () => {
+  // 消息渲染出来后再将滚动条拉到最底
+  nextTick(() => {
+    const container = document.getElementById("chat-container")
+    container.scrollTop = container.scrollHeight;
+  })
+});
+
 
 </script>
 
