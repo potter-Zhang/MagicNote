700f3b589c55bebe375758d50b00878c005a5272
susong <1592358508@qq.com>
2023-11-30 18:30:42 +0800

    minio实现文件上传，下载，删除

--- MagicNote/MagicNote/src/main/java/edu/whu/MagicNote/service/impl/MinioService.java
+++ MagicNote/MagicNote/src/main/java/edu/whu/MagicNote/service/impl/MinioService.java
@@ -11,11 +11,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.util.List;
+
 
 @Slf4j
 @Component
@@ -24,9 +26,8 @@ public class MinioService {
     private MinioClient client;
     @Autowired
     private MinioProp minioProp;
-    /**
-     * 创建bucket
-     */
+
+    //创建bucket
     public void createBucket(String bucketName) throws TodoException {
         BucketExistsArgs bucketExistsArgs = BucketExistsArgs.builder().bucket(bucketName).build();
         MakeBucketArgs makeBucketArgs = MakeBucketArgs.builder().bucket(bucketName).build();
@@ -36,33 +37,37 @@ public class MinioService {
             client.makeBucket(makeBucketArgs);
         } catch (Exception e) {
             log.error("创建桶失败：{}", e.getMessage());
-            throw new TodoException(TodoException.MINIO_ERROR,"创建桶失败");
+            throw new TodoException(TodoException.MINIO_ERROR, "创建桶失败");
         }
     }
-
+    //获取所有bucket
     public List<Bucket> getAllBuckets() throws ServerException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
         return client.listBuckets();
     }
-    /**
-     * @param file       文件
-     * @param bucketName 存储桶
-     * @return
-     */
+    //判断文件是否存在
+    public boolean fileExists(String bucketName, String fileName) throws IOException, InvalidKeyException, InvalidResponseException, InsufficientDataException, NoSuchAlgorithmException, ServerException, ErrorResponseException, InternalException, XmlParserException {
+        return client.statObject(StatObjectArgs.builder()
+                .bucket(bucketName)
+                .object(fileName)
+                .build()) != null;
+    }
+
+    //上传文件
     public JSONObject uploadFile(MultipartFile file, String bucketName) throws Exception {
         // 判断上传文件是否为空
         if (null == file || 0 == file.getSize()) {
-            throw new TodoException(TodoException.MINIO_ERROR,"文件不能为空");
+            throw new TodoException(TodoException.MINIO_ERROR, "文件不能为空");
         }
         // 判断存储桶是否存在
         createBucket(bucketName);
         // 文件名
         String originalFilename = file.getOriginalFilename();
         // 新的文件名 = 存储桶名称_时间戳.后缀名
-        String fileName = bucketName + "_" + System.currentTimeMillis() +                                                               originalFilename.substring(originalFilename.lastIndexOf("."));
+        String fileName = bucketName + "_" + System.currentTimeMillis() + originalFilename.substring(originalFilename.lastIndexOf("."));
         // 开始上传
         InputStream inputStream = file.getInputStream();
         PutObjectArgs args = PutObjectArgs.builder().bucket(bucketName).object(fileName)
-                .stream(inputStream,inputStream.available(),-1).contentType(file.getContentType()).build();
+                .stream(inputStream, inputStream.available(), -1).contentType(file.getContentType()).build();
         client.putObject(args);
         JSONObject res = new JSONObject();
         res.put("Endpoint", minioProp.getEndpoint());
@@ -70,15 +75,24 @@ public class MinioService {
         res.put("fileName", fileName);
         return res;
     }
-
-    public InputStream downloadFile(String bucketName, String objectName) throws MinioException, IOException, NoSuchAlgorithmException, InvalidKeyException {
-        return client.getObject(
-                GetObjectArgs.builder()
-                        .bucket(bucketName)
-                        .object(objectName)
-                        .build()
-        );
+    //下载文件
+    public InputStream downloadFile(String bucketName, String fileName) throws TodoException {
+        try {
+            return client.getObject(
+                    GetObjectArgs.builder()
+                            .bucket(bucketName)
+                            .object(fileName)
+                            .build()
+            );
+        } catch (Exception e) {
+            throw new TodoException(TodoException.MINIO_ERROR,"下载失败");
+        }
     }
-
-
+    //删除文件
+    public void deleteFile(String bucketName, String fileName) throws IOException, InvalidKeyException, InvalidResponseException, InsufficientDataException, NoSuchAlgorithmException, ServerException, ErrorResponseException, InternalException, XmlParserException{
+        client.removeObject(RemoveObjectArgs.builder()
+                .bucket(bucketName)
+                .object(fileName)
+                .build());
+    }
 }
