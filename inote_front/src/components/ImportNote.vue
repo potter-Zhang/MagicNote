5d867c79a904f5e78ce11e774765d4a116677608
@little__prince <13510222833@139.com>
2023-12-04 12:13:42 +0800

    音视频导入笔记完成 预览窗口加载 导入按钮合并

--- inote_front/src/components/ImportNote.vue
+++ inote_front/src/components/ImportNote.vue
@@ -5,6 +5,8 @@
   import wavesLeft from "@icon-park/vue-next/lib/icons/WavesLeft";
   import notebookIcon from "@icon-park/vue-next/lib/icons/Notebook"
 
+  import upload from "@icon-park/vue-next/lib/icons/Upload"
+
   import {ref, onMounted} from "vue";
 
   import {currentNotebooks, currentUser} from "@/global";
@@ -14,30 +16,45 @@
   import {ElMessage} from "element-plus";
   import {addNoteAPI} from "@/api/note";
   import { transformPdfAPI, transformPptAPI  } from "../api/ocr";
-
-  var pdfUploader = null
-  var pptUploader = null
+  import { transformAudioVideoAPI } from "@/api/transcribe";
+import { Loading } from "@icon-park/vue-next";
 
   const reader = new FileReader()
 
+  const fileName = ref('')
+
+  const uploading = ref(false)
+
   onMounted(() => {
-    pdfUploader = document.getElementById('file-uploader-pdf')
-    pdfUploader.addEventListener('change', (event) => {
+    var fileUploader = document.getElementById('file-uploader')
+    fileUploader.addEventListener('change', (event) => {
+      if (event.target.files.length === 0)
+        return
       const file = event.target.files[0]
+      console.log(file.type)
+      fileName.value = file.name
       const formData = new FormData()
       formData.append('file', file)
-
-      transformPdfAPI(formData).then((msg) => {previewText.value = extractPDFMeta(msg)}).catch((err) => ElMessage(err.response))
+      uploading.value = true
+      if (file.type === 'application/pdf') {
+        transformPdfAPI(formData).then((msg) => {previewText.value = extractPDFMeta(msg)}).catch((err) => ElMessage.error(err.response)).finally(() => uploading.value = false)
+      }
+      else if (file.type === 'application/vnd.openxmlformats-officedocument.presentationml.presentation') {
+        transformPptAPI(formData).then((msg) => {previewText.value = msg}).catch((err) => ElMessage.error(err.response)).finally(() => uploading.value = false)
+      }
+      else if (file.type.startsWith('audio') || file.type.startsWith('video')) {
+        transformAudioVideoAPI(formData).then((msg) => {previewText.value = msg}).catch((err) => ElMessage(err.response)).finally(() => uploading.value = false)
+      }
+      else {
+        uploading.value = false
+        ElMessage.error('文件类型不支持')
+      }
+      
+      
     })
 
-    pptUploader = document.getElementById('file-uploader-ppt')
-    pptUploader.addEventListener('change', (event) => {
-      const file = event.target.files[0]
-      const formData = new FormData()
-      formData.append('file', file)
-
-      transformPptAPI(formData).then((msg) => {previewText.value = msg}).catch((err) => ElMessage(err.response))
-    })
+    
+    
   })
 
   function extractPDFMeta(msg) {
@@ -69,11 +86,7 @@
 
   }
 
-  const previewText = ref("长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文" +
-      "本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文"+
-  "长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文"+
-  "长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文"+
-  "长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文")
+  const previewText = ref('导入你的笔记吧~')
 
   const selectedNotebook = ref("");
   const newNoteName = ref("");
@@ -110,7 +123,7 @@
 
 <template>
   <div id="import-body">
-    <!-- 选项 -->
+    <!-- 选项 
     <div id="option-bar">
       <el-button class="upload-btn" type="primary" @click="importFromPDF">
         <div style="display: flex; align-items: center">
@@ -141,6 +154,7 @@
         <input type="file" id="file-uploader">
       </el-button>
     </div>
+    -->
 
     <div style="display: flex; flex-grow: 1; flex-wrap: wrap; justify-content: center; width: 100%; ">
       <!-- 预览 -->
@@ -149,11 +163,24 @@
           <div class="rect-before"></div>
           <div>导入预览：</div>
         </div>
-        <span id="preview-text">{{ previewText }}</span>
+        <span id="preview-text" v-loading="uploading">{{ previewText }}</span>
       </div>
 
       <!-- 选择笔记本和笔记 -->
       <div id="choose-box">
+        <div class="upload-area">
+          <el-button class="upload-btn" type="primary" @click="importFromPDF">
+            <div style="display: flex; align-items: center">
+              <upload theme="outline" size="30" fill="#ffffff"/>
+              <div class="btn-text">导入</div>
+              <input type="file" id="file-uploader" accept=".pdf, .pptx, image/*, video/*">
+            </div>
+          </el-button>
+          <el-input type="textarea" resize="none" rows="1" readonly="true" v-model="fileName"></el-input>
+          <div class="file-type-prompt"> 
+            <label>支持pdf, ppt, 视频和音频导入</label>
+          </div>
+        </div>
         <div id="choose">
           <div style="display: flex; align-items: center; margin-bottom: 10px">
             <div class="rect-before"></div>
@@ -207,11 +234,16 @@
     z-index: 1;
   }
 
+  .file-name {
+    resize: none;
+  }
+
   .upload-btn {
     min-width: 150px;
-    width: 20%;
+    width: 90%;
+    aspect-ratio: 3;
     padding: 35px;
-    margin: 20px;
+    margin: 13px;
     border-radius: 10px;
     box-shadow: 0 0 3px 3px rgb(220,220,220, 0.5);
     position: relative;
@@ -221,6 +253,22 @@
     color: var(--el-button-text-color);
     background-color: var(--el-button-bg-color);
     border-color: var(--el-button-border-color);
+  }
+
+  .upload-area {
+    display: flex;
+    flex-direction: column;
+    border-radius: 5px;
+    border: 1px dashed #a5d63f;
+    padding: 20px;
+    margin-top: 10%;
+    margin-bottom: 20%;
+  }
+
+  .file-type-prompt {
+    font-size: small;
+    margin-top: 1%;
+    justify-content: center;
   }
 
   .btn-text {
@@ -234,12 +282,13 @@
     flex-direction: column;
     align-items: center;
     width: 50%;
-    height: 90%;
+    height: 80%;
     background-color: rgb(250, 250, 250);
     box-shadow: 0 0 5px 5px rgb(128,128,128,0.3);
     min-width: 500px;
     box-sizing: border-box;
     position: relative;
+    margin-top: 5%;
     overflow: scroll;
   }
 
