bd25b3e79d0bf43ffcbcc817fae7c12d77252c00
JeromeYJ <3064325896@qq.com>
2023-12-04 22:16:39 +0800

    将各个ai功能接口与问答接口改为逐字输出形式

--- MagicNote/MagicNote/src/main/java/edu/whu/MagicNote/service/impl/AIFunctionService.java
+++ MagicNote/MagicNote/src/main/java/edu/whu/MagicNote/service/impl/AIFunctionService.java
@@ -3,36 +3,62 @@ package edu.whu.MagicNote.service.impl;
 import com.alibaba.dashscope.aigc.generation.Generation;
 import com.alibaba.dashscope.aigc.generation.GenerationResult;
 import com.alibaba.dashscope.aigc.generation.models.QwenParam;
+import com.alibaba.dashscope.common.Message;
+import com.alibaba.dashscope.common.Role;
 import com.alibaba.dashscope.exception.InputRequiredException;
 import com.alibaba.dashscope.exception.NoApiKeyException;
 import com.alibaba.dashscope.utils.Constants;
 import io.github.asleepyfish.util.OpenAiUtils;
+import io.reactivex.Flowable;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
 
 @Service
 public class AIFunctionService {
     // 缩写、提炼笔记信息的方法
-    public String abstractNote(String note) throws NoApiKeyException, InputRequiredException {
+    public void abstractNote(String note, HttpServletResponse response) throws NoApiKeyException, InputRequiredException {
         Constants.apiKey = "sk-4ee81ca5526343e5b3f7c6b3baac0a85";
 
         String command = "接下来我会给出我的笔记，你需要提炼、 缩写我的笔记，尽量精简。\n" +
-                "最终输出为markdown格式，同时你需要将最重要的那些信息在markdown中进行加粗。最后只需要输出最终结果的markdown，无需其他提示信息。\n" +
+                "要求：1、最终输出为markdown格式；" +
+                "2、你需要将最重要的那些信息在markdown中进行加粗、加红色等；" +
+                "3、充分使用多级标题；" +
+                "4、最终不需要回答其他信息，返回markdown结果即可。"+
                 "给出的笔记是：\n";
 
         String prompt = command + note;
-
         Generation gen = new Generation();
-        QwenParam param = QwenParam.builder().model("qwen-max").prompt(prompt)
-                .topP(0.8).build();
-        GenerationResult result = gen.call(param);
+        Message userMsg = Message
+                .builder()
+                .role(Role.USER.getValue())
+                .content(prompt)
+                .build();
+        QwenParam param =
+                QwenParam.builder().model("qwen-max").messages(Arrays.asList(userMsg))
+                        .resultFormat(QwenParam.ResultFormat.MESSAGE)
+                        .topP(0.8)
+                        .incrementalOutput(true)
+                        .build();
+        Flowable<GenerationResult> result = gen.streamCall(param);
+        StringBuilder fullContent = new StringBuilder();
 
-        System.out.println(result.getOutput().getText());
-        return result.getOutput().getText();
+        response.setContentType("text/event-stream");
+        response.setCharacterEncoding("UTF-8");
+
+        response.setHeader("Cache-Control", "no-cache");
+
+        result.blockingForEach(message -> {
+            fullContent.append(message.getOutput().getChoices().get(0).getMessage().getContent());
+            response.getOutputStream().write(message.getOutput().getChoices().get(0).getMessage().getContent().getBytes());
+            response.getOutputStream().flush();
+        });
+        System.out.println(fullContent.toString());
     }
 
+    // 缩写笔记GPT版
     public void abstractNoteGPT(String note, HttpServletResponse response) throws IOException {
         String command = "接下来我会给出我的笔记，你需要提炼、 缩写我的笔记，尽量精简。\n" +
                 "要求：1、最终输出为markdown格式；" +
@@ -49,69 +75,176 @@ public class AIFunctionService {
         response.setHeader("Cache-Control", "no-cache");
 
         String content = command + note;
-        System.out.println("Begin");
         OpenAiUtils.createStreamChatCompletion(content, response.getOutputStream());
     }
 
     // 扩写笔记的方法
-    public String expandNote(String note) throws NoApiKeyException, InputRequiredException {
+    public void expandNote(String note, HttpServletResponse response) throws NoApiKeyException, InputRequiredException {
         Constants.apiKey = "sk-4ee81ca5526343e5b3f7c6b3baac0a85";
 
         String command = "接下来我会给出我的笔记，你需要根据关键信息扩写我的笔记，尽量详细。\n" +
-                "同时你需要将其中你认为的最重要的那些信息进行加粗，最终输出为markdown格式。最后只需要输出markdown文本。\n" +
+                "要求：1、最终输出为markdown格式；" +
+                "2、你需要将最重要的那些信息在markdown中进行加粗、加红色等；" +
+                "3、充分使用多级标题；" +
+                "4、最终不需要回答其他信息，返回markdown结果即可。"+
                 "给出的笔记是：\n";
 
         String prompt = command + note;
-
         Generation gen = new Generation();
-        QwenParam param = QwenParam.builder().model("qwen-max").prompt(prompt)
-                .topP(0.8).build();
-        GenerationResult result = gen.call(param);
+        Message userMsg = Message
+                .builder()
+                .role(Role.USER.getValue())
+                .content(prompt)
+                .build();
+        QwenParam param =
+                QwenParam.builder().model("qwen-max").messages(Arrays.asList(userMsg))
+                        .resultFormat(QwenParam.ResultFormat.MESSAGE)
+                        .topP(0.8)
+                        .incrementalOutput(true)
+                        .build();
+        Flowable<GenerationResult> result = gen.streamCall(param);
+        StringBuilder fullContent = new StringBuilder();
 
-        System.out.println(result.getOutput().getText());
-        return result.getOutput().getText();
+        response.setContentType("text/event-stream");
+        response.setCharacterEncoding("UTF-8");
+        response.setHeader("Cache-Control", "no-cache");
+
+        result.blockingForEach(message -> {
+            fullContent.append(message.getOutput().getChoices().get(0).getMessage().getContent());
+            response.getOutputStream().write(message.getOutput().getChoices().get(0).getMessage().getContent().getBytes());
+            response.getOutputStream().flush();
+        });
+        System.out.println(fullContent.toString());
+    }
+
+    public void expandNoteGPT(String note, HttpServletResponse response) throws IOException {
+        String command = "接下来我会给出我的笔记，你需要根据关键信息扩写我的笔记，尽量详细。\n" +
+                "要求：1、最终输出为markdown格式；" +
+                "2、你需要将最重要的那些信息在markdown中进行加粗、加红色等；" +
+                "3、充分使用多级标题；" +
+                "4、最终不需要回答其他信息，返回markdown结果即可。"+
+                "给出的笔记是：\n";
+
+        response.setContentType("text/event-stream");
+        response.setCharacterEncoding("UTF-8");
+        response.setHeader("Cache-Control", "no-cache");
+
+        String content = command + note;
+        OpenAiUtils.createStreamChatCompletion(content, response.getOutputStream());
     }
 
     // 根据关键词生成笔记的方法
-    public String generateNote(String words, int num) throws NoApiKeyException, InputRequiredException {
+    public void generateNote(String words, int num, HttpServletResponse response) throws NoApiKeyException, InputRequiredException {
         Constants.apiKey = "sk-4ee81ca5526343e5b3f7c6b3baac0a85";
 
         String command = "接下来我会给出一些关键词，这些关键词使用分号间隔，你需要根据这些关键词生成一篇" + num + "字以上的中文笔记，尽量详细。\n" +
-                "同时你需要将其中你认为的最重要的那些信息进行加粗，最终输出为markdown格式。最后只需要输出markdown文本。\n" +
+                "要求：1、最终输出为markdown格式；" +
+                "2、你需要将最重要的那些信息在markdown中进行加粗、加红色等；" +
+                "3、充分使用多级标题；" +
+                "4、最终不需要回答其他信息，返回markdown结果即可。"+
                 "给出的关键词是：\n";
 
         String prompt = command + words;
-
         Generation gen = new Generation();
-        QwenParam param = QwenParam.builder().model("qwen-max").prompt(prompt)
-                .topP(0.8).build();
-        GenerationResult result = gen.call(param);
+        Message userMsg = Message
+                .builder()
+                .role(Role.USER.getValue())
+                .content(prompt)
+                .build();
+        QwenParam param =
+                QwenParam.builder().model("qwen-max").messages(Arrays.asList(userMsg))
+                        .resultFormat(QwenParam.ResultFormat.MESSAGE)
+                        .topP(0.8)
+                        .incrementalOutput(true)
+                        .build();
+        Flowable<GenerationResult> result = gen.streamCall(param);
+        StringBuilder fullContent = new StringBuilder();
 
-        System.out.println(result.getOutput().getText());
-        return result.getOutput().getText();
+        response.setContentType("text/event-stream");
+        response.setCharacterEncoding("UTF-8");
+        response.setHeader("Cache-Control", "no-cache");
+
+        result.blockingForEach(message -> {
+            fullContent.append(message.getOutput().getChoices().get(0).getMessage().getContent());
+            response.getOutputStream().write(message.getOutput().getChoices().get(0).getMessage().getContent().getBytes());
+            response.getOutputStream().flush();
+        });
+        System.out.println(fullContent.toString());
+    }
+
+    public void generateNoteGPT(String words, int num, HttpServletResponse response) throws IOException {
+        String command = "接下来我会给出一些关键词，这些关键词使用分号间隔，你需要根据这些关键词生成一篇" + num + "字以上的中文笔记，尽量详细。\n" +
+                "要求：1、最终输出为markdown格式；" +
+                "2、你需要将最重要的那些信息在markdown中进行加粗、加红色等；" +
+                "3、充分使用多级标题；" +
+                "4、最终不需要回答其他信息，返回markdown结果即可。"+
+                "给出的关键词是：\n";
+
+        response.setContentType("text/event-stream");
+        response.setCharacterEncoding("UTF-8");
+        response.setHeader("Cache-Control", "no-cache");
+
+        String content = command + words;
+        OpenAiUtils.createStreamChatCompletion(content, response.getOutputStream());
     }
 
     // 将笔记分段
-    public String segmentNote(String note) throws NoApiKeyException, InputRequiredException {
+    public void segmentNote(String note, HttpServletResponse response) throws NoApiKeyException, InputRequiredException {
         Constants.apiKey = "sk-4ee81ca5526343e5b3f7c6b3baac0a85";
 
-        String command = "接下来我会给出一篇笔记，请将其根据内容使用各级标题进行合理的分段。\n" +
-                "最终输出为markdown格式。最后只需要输出markdown文本。\n" +
+        String command = "接下来我会给出一篇笔记，请将其根据内容使用各级标题进行合理的分段.\n" +
+                "要求：1、最终输出为markdown格式；" +
+                "2、你需要将最重要的那些信息在markdown中进行加粗、加红色等；" +
+                "3、充分使用多级标题；" +
+                "4、最终不需要回答其他信息，返回markdown结果即可。"+
                 "给出的笔记是：\n";
 
         String prompt = command + note;
-
         Generation gen = new Generation();
-        QwenParam param = QwenParam.builder().model("qwen-max").prompt(prompt)
-                .topP(0.8).build();
-        GenerationResult result = gen.call(param);
+        Message userMsg = Message
+                .builder()
+                .role(Role.USER.getValue())
+                .content(prompt)
+                .build();
+        QwenParam param =
+                QwenParam.builder().model("qwen-max").messages(Arrays.asList(userMsg))
+                        .resultFormat(QwenParam.ResultFormat.MESSAGE)
+                        .topP(0.8)
+                        .incrementalOutput(true)
+                        .build();
+        Flowable<GenerationResult> result = gen.streamCall(param);
+        StringBuilder fullContent = new StringBuilder();
 
-        System.out.println(result.getOutput().getText());
-        return result.getOutput().getText();
+        response.setContentType("text/event-stream");
+        response.setCharacterEncoding("UTF-8");
+        response.setHeader("Cache-Control", "no-cache");
+
+        result.blockingForEach(message -> {
+            fullContent.append(message.getOutput().getChoices().get(0).getMessage().getContent());
+            response.getOutputStream().write(message.getOutput().getChoices().get(0).getMessage().getContent().getBytes());
+            response.getOutputStream().flush();
+        });
+        System.out.println(fullContent.toString());
+    }
+
+    public void segmentNoteGPT(String note, HttpServletResponse response) throws IOException {
+        String command = "接下来我会给出一篇笔记，请将其根据内容使用各级标题进行合理的分段.\n" +
+                "要求：1、最终输出为markdown格式；" +
+                "2、你需要将最重要的那些信息在markdown中进行加粗、加红色等；" +
+                "3、充分使用多级标题；" +
+                "4、最终不需要回答其他信息，返回markdown结果即可。"+
+                "给出的笔记是：\n";
+
+        response.setContentType("text/event-stream");
+        response.setCharacterEncoding("UTF-8");
+        response.setHeader("Cache-Control", "no-cache");
+
+        String content = command + note;
+        OpenAiUtils.createStreamChatCompletion(content, response.getOutputStream());
     }
 
     // 根据笔记的合适内容生成表格
-    public String generateTable(String note) throws NoApiKeyException, InputRequiredException {
+    public void generateTable(String note, HttpServletResponse response) throws NoApiKeyException, InputRequiredException {
         Constants.apiKey = "sk-a513d206b66948ad8b27356775c8c829";
 
         String command ="请根据我的笔记，生成结构清晰的makedown格式的表格。\n" +
@@ -120,19 +253,53 @@ public class AIFunctionService {
                 "3、表格内容能够保持和表格列标题一致时，用一个表格概括；否则可以用多个表格概括，但必须都要保持表格的格式"+
                 "4、使用Markdown的表格语法创建表格。" +
                 "给出的笔记是：\n";
+
         String prompt = command + note;
-
         Generation gen = new Generation();
-        QwenParam param = QwenParam.builder().model("qwen-max").prompt(prompt)
-                .topP(0.8).build();
-        GenerationResult result = gen.call(param);
+        Message userMsg = Message
+                .builder()
+                .role(Role.USER.getValue())
+                .content(prompt)
+                .build();
+        QwenParam param =
+                QwenParam.builder().model("qwen-max").messages(Arrays.asList(userMsg))
+                        .resultFormat(QwenParam.ResultFormat.MESSAGE)
+                        .topP(0.8)
+                        .incrementalOutput(true)
+                        .build();
+        Flowable<GenerationResult> result = gen.streamCall(param);
+        StringBuilder fullContent = new StringBuilder();
 
-        System.out.println(result.getOutput().getText());
-        return result.getOutput().getText();
+        response.setContentType("text/event-stream");
+        response.setCharacterEncoding("UTF-8");
+        response.setHeader("Cache-Control", "no-cache");
+
+        result.blockingForEach(message -> {
+            fullContent.append(message.getOutput().getChoices().get(0).getMessage().getContent());
+            response.getOutputStream().write(message.getOutput().getChoices().get(0).getMessage().getContent().getBytes());
+            response.getOutputStream().flush();
+        });
+        System.out.println(fullContent.toString());
+    }
+
+    public void generateTableGPT(String note, HttpServletResponse response) throws IOException {
+        String command ="请根据我的笔记，生成结构清晰的makedown格式的表格。\n" +
+                "要求：1、保持表格的清晰排列和标签" +
+                "2、根据笔记的内容主旨生成简短扼要的表格标题" +
+                "3、表格内容能够保持和表格列标题一致时，用一个表格概括；否则可以用多个表格概括，但必须都要保持表格的格式"+
+                "4、使用Markdown的表格语法创建表格。" +
+                "给出的笔记是：\n";
+
+        response.setContentType("text/event-stream");
+        response.setCharacterEncoding("UTF-8");
+        response.setHeader("Cache-Control", "no-cache");
+
+        String content = command + note;
+        OpenAiUtils.createStreamChatCompletion(content, response.getOutputStream());
     }
 
     // 根据笔记的合适内容生成流程图
-    public String generateFlowChart(String note) throws NoApiKeyException, InputRequiredException {
+    public void generateFlowChart(String note, HttpServletResponse response) throws NoApiKeyException, InputRequiredException {
         Constants.apiKey = "sk-a513d206b66948ad8b27356775c8c829";
 
         String command ="请从我的笔记中提取出关键词，并通过合理的逻辑关系以makedowm格式的mermaid流程图表示。\n" +
@@ -140,14 +307,45 @@ public class AIFunctionService {
                 "2、使用Markdown语法创建流程图"+
                 "给出的笔记是：\n";
         String prompt = command + note;
-
         Generation gen = new Generation();
-        QwenParam param = QwenParam.builder().model("qwen-max").prompt(prompt)
-                .topP(0.8).build();
-        GenerationResult result = gen.call(param);
+        Message userMsg = Message
+                .builder()
+                .role(Role.USER.getValue())
+                .content(prompt)
+                .build();
+        QwenParam param =
+                QwenParam.builder().model("qwen-max").messages(Arrays.asList(userMsg))
+                        .resultFormat(QwenParam.ResultFormat.MESSAGE)
+                        .topP(0.8)
+                        .incrementalOutput(true)
+                        .build();
+        Flowable<GenerationResult> result = gen.streamCall(param);
+        StringBuilder fullContent = new StringBuilder();
 
-        System.out.println(result.getOutput().getText());
-        return result.getOutput().getText();
+        response.setContentType("text/event-stream");
+        response.setCharacterEncoding("UTF-8");
+        response.setHeader("Cache-Control", "no-cache");
+
+        result.blockingForEach(message -> {
+            fullContent.append(message.getOutput().getChoices().get(0).getMessage().getContent());
+            response.getOutputStream().write(message.getOutput().getChoices().get(0).getMessage().getContent().getBytes());
+            response.getOutputStream().flush();
+        });
+        System.out.println(fullContent.toString());
+    }
+
+    public void generateFlowChartGPT(String note, HttpServletResponse response) throws IOException {
+        String command ="请从我的笔记中提取出关键词，并通过合理的逻辑关系以makedowm格式的mermaid流程图表示。\n" +
+                "要求：1、类似的结点要多进行分支的流程图表示"+
+                "2、使用Markdown语法创建流程图"+
+                "给出的笔记是：\n";
+
+        response.setContentType("text/event-stream");
+        response.setCharacterEncoding("UTF-8");
+        response.setHeader("Cache-Control", "no-cache");
+
+        String content = command + note;
+        OpenAiUtils.createStreamChatCompletion(content, response.getOutputStream());
     }
 
     // 对多模态识别结果进行错误修改
@@ -169,6 +367,5 @@ public class AIFunctionService {
         System.out.println(result.getOutput().getText());
         return result.getOutput().getText();
     }
-
 
 }
