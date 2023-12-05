f50a80bc4baa9831a2e55689fc6e3fc2e57452a0
@little__prince <13510222833@139.com>
2023-12-05 18:12:06 +0800

    unstable

--- MagicNote/MagicNote/src/main/java/edu/whu/MagicNote/controller/AIFunctionController.java
+++ MagicNote/MagicNote/src/main/java/edu/whu/MagicNote/controller/AIFunctionController.java
@@ -124,14 +124,56 @@ public class AIFunctionController {
     @PostMapping("/generateTableGPT")
     public ResponseEntity<Void> generateTableGPT(@RequestBody AIObject aiObject, HttpServletResponse response) {
         try {
-            aiFunctionService.generateTableGPT(aiObject.getStr(),response);
-            return ResponseEntity.ok().build();
-        } catch (IOException e) {
-            return ResponseEntity.badRequest().build();
-        }
-    }
-
     // 根据笔记的合适内容生成流程图
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     @PostMapping("/generateFlowChart")
     public ResponseEntity<Void> generateFlowChart(@RequestBody AIObject aiObject, HttpServletResponse response) throws NoApiKeyException, InputRequiredException {
         try {
@@ -160,8 +202,8 @@ public class AIFunctionController {
     }
 
     // 回答问题时调用的接口
-    @PostMapping("/answer")
-    public ResponseEntity<String> getAnswer(@RequestBody AIObject aiObject, HttpServletResponse response) {
+    @GetMapping("/answer")
+    public ResponseEntity<String> getAnswer(AIObject aiObject, HttpServletResponse response) {
         try {
             qAndAService.answer(aiObject.getStr(),response);
             return ResponseEntity.ok().build();
@@ -170,107 +212,4 @@ public class AIFunctionController {
         }
     }
 
-    @GetMapping(value = "/streamAsk", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public Flux<ServerSentEvent<String>> streamCallWithMessage(String q, HttpServletResponse response)
-            throws NoApiKeyException, ApiException, InputRequiredException {
-        Constants.apiKey = "sk-4ee81ca5526343e5b3f7c6b3baac0a85";
-
-        String command = "接下来我会给出我的笔记，你需要提炼、 缩写我的笔记，尽量精简。\n" +
-                "最终输出为markdown格式，同时你需要将最重要的那些信息在markdown中进行加粗。最后只需要输出最终结果的markdown，无需其他提示信息。\n" +
-                "给出的笔记是：\n";
-
-        String prompt = command + q;
-
-        Generation gen = new Generation();
-
-        Message userMsg = Message
-                .builder()
-                .role(Role.USER.getValue())
-                .content(prompt)
-                .build();
-        QwenParam param =
-                QwenParam.builder().model("qwen-max").messages(Arrays.asList(userMsg))
-                        .resultFormat(QwenParam.ResultFormat.MESSAGE)
-                        .topP(0.8)
-                        .incrementalOutput(true) // get streaming output incrementally
-                        .build();
-        Flowable<GenerationResult> result = gen.streamCall(param);
-
-        StringBuilder fullContent = new StringBuilder();
-        System.out.println(fullContent.toString());
-        return Flux.from(result)
-                // add delay between each event
-                .delayElements(Duration.ofMillis(1000))
-                .map(message -> {
-                    String output = message.getOutput().getChoices().get(0).getMessage().getContent();
-                    System.out.println(output); // print the output
-                    return ServerSentEvent.<String>builder()
-                            .data(output)
-                            .build();
-                })
-                .concatWith(Flux.just(ServerSentEvent.<String>builder().comment("").build()))
-                .doOnError(e -> {
-                    if (e instanceof NoApiKeyException) {
-                        // 处理 NoApiKeyException
-                    } else if (e instanceof InputRequiredException) {
-                        // 处理 InputRequiredException
-                    } else if (e instanceof ApiException) {
-                        // 处理其他 ApiException
-                    } else {
-                        // 处理其他异常
-                    }
-                });
-    }
-
-    @GetMapping(value = "/streamQwen", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public void streamQwen(String q, HttpServletResponse response)
-            throws NoApiKeyException, ApiException, InputRequiredException {
-        Constants.apiKey = "sk-4ee81ca5526343e5b3f7c6b3baac0a85";
-
-        String command = "接下来我会给出我的笔记，你需要提炼、 缩写我的笔记，尽量精简。\n" +
-                "要求：1、最终输出为markdown格式；" +
-                "2、你需要将最重要的那些信息在markdown中进行加粗、加红色等；" +
-                "3、充分使用多级标题；" +
-                "4、最终不需要回答其他信息，返回markdown结果即可。"+
-                "给出的笔记是：\n";
-
-        String prompt = command + q;
-
-        Generation gen = new Generation();
-
-        Message userMsg = Message
-                .builder()
-                .role(Role.USER.getValue())
-                .content(prompt)
-                .build();
-        QwenParam param =
-                QwenParam.builder().model("qwen-max").messages(Arrays.asList(userMsg))
-                        .resultFormat(QwenParam.ResultFormat.MESSAGE)
-                        .topP(0.8)
-                        .incrementalOutput(true) // get streaming output incrementally
-                        .build();
-        Flowable<GenerationResult> result = gen.streamCall(param);
-        StringBuilder fullContent = new StringBuilder();
-
-        // 需要指定response的ContentType为流式输出，且字符编码为UTF-8
-        response.setContentType("text/event-stream");
-        response.setCharacterEncoding("UTF-8");
-
-        // 禁用缓存
-        response.setHeader("Cache-Control", "no-cache");
-
-        result.blockingForEach(message -> {
-            fullContent.append(message.getOutput().getChoices().get(0).getMessage().getContent());
-            response.getOutputStream().write(message.getOutput().getChoices().get(0).getMessage().getContent().getBytes());
-            response.getOutputStream().flush();
-        });
-        System.out.println("Full content: \n" + fullContent.toString());
-        System.out.println(fullContent.toString());
-    }
-
-    @GetMapping("/streamChatWithWeb")
-    public void streamChatWithWeb(String content, HttpServletResponse response) throws IOException {
-
-        aiFunctionService.abstractNoteGPT(content, response);
-    }
 }
